6758c48637614bca21bd863354f0852353274864
Tomasz Sokolowski <sokolowskit@gmail.com>
2018-05-05 15:50:26 +0200

    Update App.js file to JSX

--- components/App.js
+++ components/App.js
@@ -32,17 +32,3 @@ var App = React.createClass({
 			)
 		}
 	});
-
-			/*/*React.createElement('div', {
-					className: 'app'
-				},
-				React.createElement(ContactForm, {
-					contact: contactForm
-				}),
-				React.createElement(Contacts, {
-					items: contacts
-				}, {})
-			)
-		);
-	}
-});*/*/
